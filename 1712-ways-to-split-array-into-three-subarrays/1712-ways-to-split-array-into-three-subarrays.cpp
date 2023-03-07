332f843c244246059cdd95ada7763d05c74b1705
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-03-07 11:42:45 -0800

    Time: 154 ms (92.39%), Space: 85 MB (59.78%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int waysToSplit(vector<int>& nums) {
-        vector<int> pre(nums);
-        pre[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
-            pre[i]=pre[i-1]+nums[i];
-        }
-        int res=0,mod=1e9+7;
-        //given that:
-        //i is the end of first array
-        //j is the start of second subarray
-        //k is end of second subarray
-        //if we maintain the mid array we can find other two ones
-        //preSum[i]<=preSum[j]-preSum[i]<=preSum[n-1]-preSum[j]
-        //First: preSum[i]*2<=preSum[j]
-        //second: preSum[j]*2<=preSum[n-1]+preSum[i]
+        vector<int> prefix(nums);
         int n=nums.size();
-        // we should have atleast one element in each subarray
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+        //preSum[i]<=preSum[j]-preSum[i]<=preSum[n-1]-preSum[j]
+        //preSum[i]*2<=preSum[j] and preSum[j]*2<=preSum[n-1]+preSum[i]
+        int result=0,mod=1e9+7;
         for(int i=0,j=0,k=0;i<n-2;i++){
-            while(j<=i ||(j<n-1 && 2*pre[i]>pre[j])){
+            while(j<=i || (j<n-1 && prefix[i]*2>prefix[j])){
                 j++;
             }
-            while(k<j||(k<n-1 && 2*pre[k]<=pre[n-1]+pre[i])){
+            //second condition is satisfied 
+            while(k<j || (k<n-1 && prefix[k]*2<=prefix[n-1]+prefix[i])){
                 k++;
             }
             k--;
-            res=(res+k-j+1)%mod;
+            result=(result+k-j+1)%mod;
         }
-        return res;
+        return result;
+
     }
 };
