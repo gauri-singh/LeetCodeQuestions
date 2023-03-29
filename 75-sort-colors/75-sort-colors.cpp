b176b9afb3ab6a2c7c2f594d7e9fc7fdef6953a4
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-03-29 15:15:21 -0700

    Time: 0 ms (100.00%), Space: 8.3 MB (32.19%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-    int ones=0, zeroes=0,twos=0;
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==0) zeroes++;
-            if(nums[i]==1) ones++;
-            if(nums[i]==2) twos++;
+    // int ones=0, zeroes=0,twos=0;
+    //     for(int i =0;i<nums.size();i++){
+    //         if(nums[i]==0) zeroes++;
+    //         if(nums[i]==1) ones++;
+    //         if(nums[i]==2) twos++;
+    //     }
+    //     nums.clear();
+    //     while(zeroes){
+    //         nums.push_back(0);
+    //         zeroes--;
+    //     }
+    //     while(ones){
+    //         nums.push_back(1);
+    //         ones--;
+    //     }
+    //     while(twos){
+    //         nums.push_back(2);
+    //         twos--;
+    //     }
+        int p0,curr,p2;
+        p0=0, p2= nums.size()-1;
+        curr =0;
+        while(curr<=p2){
+            if(nums[curr]==0){
+                swap(nums[curr],nums[p0]);
+                curr++;
+                p0++;
+            }
+            else if(nums[curr]==2){
+                swap(nums[curr],nums[p2]);
+                p2--;
+            }
+            else{
+                curr++;
+            }
         }
-        nums.clear();
-        while(zeroes){
-            nums.push_back(0);
-            zeroes--;
-        }
-        while(ones){
-            nums.push_back(1);
-            ones--;
-        }
-        while(twos){
-            nums.push_back(2);
-            twos--;
-        }
-    }
+     }
+    
 };
