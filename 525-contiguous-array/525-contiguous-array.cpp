88615b314f3dc554e498950bf24eb6fd989814ff
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-04-05 13:00:40 -0700

    Time: 133 ms (80.90%), Space: 83.8 MB (84.82%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -4,10 +4,14 @@ public:
         unordered_map <int,int> map;
         unordered_map <int,int> :: iterator it;
         int sum=0,longestSub=0,currSub;
+        //if number is 1, we add +1
+        //if number is 0 we subtract -1
+        //if any time sum =0 , it means perfectly balanced 0s and 1s at this point
+        //if any sum number repeats, this means all the elements between these two points have a sum of equal to 0, means have balanced 1s and zeores between them 
         for(int i=0;i<nums.size();i++){
             sum+=nums[i]==1?1:-1;
             if(sum==0){
-                //include all the 
+                //sum fro
                 longestSub=max(longestSub,i+1);
                 continue;
             }
