615bedf122faed7855117b764a0ff1e56cf60857
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-04-07 12:15:42 -0700

    Time: 102 ms (45.05%), Space: 41.5 MB (60.04%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // prefix sum : count -> map
+        unordered_map<int,int>map;
+        unordered_map<int,int> :: iterator it;
+        int sum =0,cnt=0,target;
+        //base case empty sub array, when you're taking the entire subarray 
+        map[0]=1;
+        for(int i =0;i<nums.size();i++){
+            sum+=nums[i];
+            target =sum-k;
+            //since we are going linearly and not pre calculating the prefix sum
+            // we know that the target would exist only if it is in the subarray
+            it = map.find(target);
+            if(it!=map.end()){
+                cnt+=map[target];
+            }
+            map[sum]++;
+        }
+        return cnt;
+    }
+};
