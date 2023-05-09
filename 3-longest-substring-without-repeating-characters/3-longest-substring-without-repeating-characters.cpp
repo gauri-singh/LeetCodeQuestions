6efaf93f07086ecf896ba209f8c6b8fc83294e88
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-05-09 13:53:21 -0700

    Time: 894 ms (6.95%), Space: 149.3 MB (7.69%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -18,5 +18,20 @@ public:
             
         }
         return ans;
+        
     }
+    /*int lengthOfLongestSubstring(string s) {
+        int maxCount =0;
+        unordered_set <char> set;
+        int left =0;int right =0;
+        for(;right<s.length();right++){
+            while(set.count(s[right])){
+                set.erase(s[left]);
+                left++;
+            }
+                set.insert(s[right]);
+            maxCount=max(maxCount,(right-left+1));
+        }
+        return maxCount;
+    }*/
 };
