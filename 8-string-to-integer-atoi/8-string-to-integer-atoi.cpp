c13a26a5f984a51a8ceeeffaed573053edb7c7b9
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-05-14 20:03:52 -0700

    Time: 27 ms (7.05%), Space: 7.1 MB (21.67%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        char sign='+';
+        int ans=0;
+        int mod = 1e9+7;
+        string num;
+        bool signEncountered=false;
+        for(auto ch:s){
+            if(ch=='-'|| ch =='+'){
+                if(signEncountered || num.length() > 0){ // sign character encountered after digits or another sign
+                    break;
+                }
+                sign = ch;
+                signEncountered = true;
+            }
+            else if(ch>='0' && ch<='9'){
+                num+=ch;
+            }
+            else if(ch==' '){
+                if(signEncountered || num.length() > 0){ // space encountered after digits or another sign
+                    break;
+                }
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        cout<<num<<endl;
+        cout<<"this is ans "<<ans<<endl;
+        for(int i=0;i<num.length();i++){
+            int digit =num[i]-'0';
+           if(ans > INT_MAX/10 || (ans == INT_MAX/10 && digit > INT_MAX%10)){
+                ans = sign=='-' ? INT_MIN : INT_MAX;
+                return ans;
+            }
+            ans=(ans*10+digit);
+            cout<<"this is ans "<<ans<<endl;
+        }
+        ans = sign=='-'? ans*-1: ans;
+        return ans;
+    }
+};
