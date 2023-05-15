582228ee8f74cdce81788627ac2fc39a97086e3f
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-05-14 21:10:24 -0700

    Time: 42 ms (24.91%), Space: 8.2 MB (28.45%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t == "" || s.length() < t.length()) return "";
-
-        unordered_map<char, int> tMap, window;
-        for (auto it : t) {
+        if (t == "" || s.length()<t.length()) return "";
+        unordered_map <char,int> tMap,window;
+        for(auto it:t){
             tMap[it]++;
         }
-
-        int need = tMap.size(), have = 0;
-        int left = 0, right = 0, resLen = INT_MAX;
-        vector<int> res(2, 0);
-
-        for (; right < s.size(); right++) {
-            char ch = s[right];
+        //need should have UNIQUE characters in t, therefore:
+        int need=tMap.size(),have=0; 
+        int left=0,right=0,resLen=INT_MAX;
+        vector <int> res(2,0);
+        for(;right<s.size();right++){
+            char ch =s[right];
             window[ch]++;
-            if (tMap.find(ch) != tMap.end() && window[ch] == tMap[ch]) {
+            if(tMap.find(ch)!=tMap.end() && window[ch]==tMap[ch]){
                 have++;
             }
-            while (have == need) {
-                if (right - left + 1 < resLen) {
-                    res = {left, right};
-                    resLen = right - left + 1;
+            while(have==need){
+                if(right-left+1 < resLen){
+                    res={left,right};
+                    resLen=right-left+1;
                 }
+                //if the condition is met we keep shrinking from left
                 window[s[left]]--;
-                if (tMap.find(s[left]) != tMap.end() && window[s[left]] < tMap[s[left]]) {
-                    have--;
-                }
-                left++;
+                //did we make it less after removing the character
+                if(tMap.find(s[left])!=tMap.end() && window[s[left]]<tMap[s[left]]){
+                have--;
             }
+                left++;
+            }//while loop
         }
-
-        if (resLen == INT_MAX) {
+        if(resLen==INT_MAX)
             return "";
-        } else {
-            return s.substr(res[0], res[1] - res[0] + 1);
-        }
+        return s.substr(res[0],res[1]-res[0]+1);
+
     }
 };
