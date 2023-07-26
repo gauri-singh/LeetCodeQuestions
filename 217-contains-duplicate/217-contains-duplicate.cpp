ef939bd45af8349fa77520f2fd7bb33d98cd367a
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2023-07-25 20:14:51 -0700

    Time: 158 ms (44.13%), Space: 69.4 MB (61.07%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,26 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> map;
-        unordered_map<int,int>:: iterator it;
         for(auto it:nums){
             map[it]++;
             if(map[it]>=2){
                 return true;
             }
         }
-        return false;
-        // for(int i = 0;i< nums.size();i++){
-        //     it= map.find(nums[i]);
-        //     if(it == map.end()){
-        //         map[nums[i]] = 1;
-        //     }
-        //     else
-        //         map[nums[i]]++;
-        // }
-        // for(auto it : map){
-        //     if(it.second > 1 )
-        //         return true;
-        // }
         return false;
     }
 };
