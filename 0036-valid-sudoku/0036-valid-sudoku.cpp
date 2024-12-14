13ef3a1d522c787579692abce744b2efbf511ca0
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2024-12-14 13:11:17 +0530

    Time: 8 ms (29.44%), Space: 26.3 MB (8.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int,unordered_set<char>>row,col;
-        map<pair<int,int>,unordered_set<char>> subgrid;
+        //maps of key where key is the row number and value is unordered_set for each row
+        //similarly for columns
+        unordered_map <int,unordered_set<char>>row,col;
+        //unordered map cant have pair as its hash key without a custom operator 
+        //map uses red black binary tree therefore we can use that 
+        map<pair<int,int>,unordered_set<char>> square;
         char ch;
         for(int r=0;r<9;r++){
-           for(int c=0;c<9;c++) {
-               ch=board[r][c];
-               if(ch=='.'){
-                   continue;
-               }
-               if(row[r].count(ch)){
-                   return false;
-               }
-                if(col[c].count(ch)){
-                   return false;
-               }
-               pair<int,int> subgridKey={r/ 3,c/ 3};
-                if(subgrid[subgridKey].count(ch)){
-                   return false;
-               }
-               row[r].insert(ch);
-               col[c].insert(ch);
-               subgrid[subgridKey].insert(ch); 
-           }
+            for(int c=0;c<9;c++){
+                ch= board[r][c];
+                pair<int, int> squareKey = {r/ 3,c/ 3};
+                if(ch=='.') continue;
+                if(row[r].count(ch)){
+                    return false;
+                }
+                else if(col[c].count(ch)){
+                    return false;
+                }
+                else if(square[squareKey].count(ch)){
+                    return false;
+                }
+                row[r].insert(ch);
+                col[c].insert(ch);
+                square[squareKey].insert(ch);
+            }
         }
         return true;
     }
