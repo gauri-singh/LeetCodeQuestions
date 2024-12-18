70e0ffebb641d1a7f4b9ee84988f415a5f173902
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2024-12-18 12:00:30 +0530

    Time: 24 ms (76.32%), Space: 107.1 MB (33.22%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,36 +1,38 @@
+
+//using pair of index and value
 // class Solution {
 // public:
 //     vector<int> dailyTemperatures(vector<int>& temperatures) {
-//         stack<int> st;
-//         // index based stack 
-//         vector<int> ans(temperatures.size(),0);
-//         st.push(0);
-//         for(int i=1;i<temperatures.size();i++){
-//             while(!st.empty() && temperatures[i]>temperatures[st.top()]){
-//                 int pos=st.top();
-//                 ans[pos]=i-pos;
+//         int n=temperatures.size();
+//         vector<int> ans(n,0);
+//         stack<pair<int,int>> st;
+//         st.push({temperatures[0],0});
+//         int j=0;
+//         for(int i=1;i<n;i++){
+//             while(!st.empty() && temperatures[i]>st.top().first){
+//                 ans[st.top().second]=i-st.top().second;
 //                 st.pop();
 //             }
-//             st.push(i);
+//             st.push({temperatures[i],i});
 //         }
 //         return ans;
 //     }
 // };
-//using pair of index and value
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n=temperatures.size();
-        vector<int> ans(n,0);
-        stack<pair<int,int>> st;
-        st.push({temperatures[0],0});
-        int j=0;
-        for(int i=1;i<n;i++){
-            while(!st.empty() && temperatures[i]>st.top().first){
-                ans[st.top().second]=i-st.top().second;
+        stack<int> st;
+        // index based stack 
+        vector<int> ans(temperatures.size(),0);
+        st.push(0);
+        for(int i=1;i<temperatures.size();i++){
+            while(!st.empty() && temperatures[i]>temperatures[st.top()]){
+                int pos=st.top();
+                ans[pos]=i-pos;
                 st.pop();
             }
-            st.push({temperatures[i],i});
+            st.push(i);
         }
         return ans;
     }
