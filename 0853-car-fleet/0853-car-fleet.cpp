c831d6d4dd5e32572055405565a4ab0edee2511d
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2024-12-18 20:22:55 +0530

    Time: 39 ms (67.42%), Space: 82.5 MB (61.34%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,21 +1,48 @@
+//using stack
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         vector<pair<int,int>> combined;
-        int n=speed.size();
+        int n=position.size();
         for(int i=0;i<n;i++){
             combined.push_back({position[i],speed[i]});
         }
         sort(combined.begin(),combined.end());
-        double prevTime= (double)(target-combined[n-1].first)/combined[n-1].second;
-        int fleet=1;
+        stack <double> st;
+        double time= (double)(target - combined[n-1].first)/combined[n-1].second;
+        st.push(time);
         for(int i=n-2;i>=0;i--){
-            double currTime=(double)(target-combined[i].first)/combined[i].second;
-            if(currTime>prevTime){
-                fleet++;
-                prevTime=currTime;
+            time= (double)(target - combined[i].first)/combined[i].second;
+            if(!st.empty() && st.top()>=time){
+                continue;
+            }
+            else{
+                st.push(time);
             }
         }
-        return fleet;
+        return st.size();
     }
 };
+
+
+// class Solution {
+// public:
+//     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+//         vector<pair<int,int>> combined;
+//         int n=speed.size();
+//         for(int i=0;i<n;i++){
+//             combined.push_back({position[i],speed[i]});
+//         }
+//         sort(combined.begin(),combined.end());
+//         double prevTime= (double)(target-combined[n-1].first)/combined[n-1].second;
+//         int fleet=1;
+//         for(int i=n-2;i>=0;i--){
+//             double currTime=(double)(target-combined[i].first)/combined[i].second;
+//             if(currTime>prevTime){
+//                 fleet++;
+//                 prevTime=currTime;
+//             }
+//         }
+//         return fleet;
+//     }
+// };
