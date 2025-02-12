de28e9daff8bb6736a6f6e16f70dd985ee29bf2e
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-02-11 19:43:10 -0800

    Time: 63 ms (32.32%), Space: 93.8 MB (87.84%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        //if two intervals are overlapping we remove the interval with smaller end point because it has less chances of overlapping with other intervals
+        
+        //algorithm
+        //sort the array
+        //compare adjacent pairs: check if overlapping
+        //if overlapping: remove the one with larger end interval value
         sort(intervals.begin(),intervals.end());
         int res=0,lastEnd=intervals[0][1],start,end;
         for(int i=1;i<intervals.size();i++){
