40e2f91c6134297df08c719072ab10f28a1fa741
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-02-16 16:09:42 -0800

    Time: 11 ms (98.77%), Space: 26.4 MB (32.15%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
@@ -1,7 +1,7 @@
 class TicTacToe {
 public:
-   // rows[row][0] tracks Player 1's moves in row 'row'
-   // rows[row][1] tracks Player 2's moves in row 'row'
+    // rows[row][0] tracks Player 1's moves in row 'row'
+    // rows[row][1] tracks Player 2's moves in row 'row'
     vector<vector<int>> rows;
     // cols[col][0] tracks Player 1's moves in column 'col'
     // cols[col][1] tracks Player 2's moves in column 'col'
@@ -15,33 +15,30 @@ public:
 
     int n; // Size of the board (n x n)
     TicTacToe(int n) {
-        this->n=n;
-        rows=vector<vector<int>>(n,vector<int>(2,0));
-        cols=vector<vector<int>>(n,vector<int>(2,0));
-        d1=vector<int>(n,0);
-        d2=vector<int>(n,0);
+        this->n = n;
+        rows = vector<vector<int>>(n, vector<int>(2, 0));
+        cols = vector<vector<int>>(n, vector<int>(2, 0));
+        d1 = vector<int>(n, 0);
+        d2 = vector<int>(n, 0);
     }
-    
+
     int move(int row, int col, int player) {
-        player--;// so that it becomes 0 indexed
-       rows[row][player]++;
-       cols[col][player]++;
-       if(row==col){
-        d1[player]++;
-       }
-       if(row+col==n-1){
-        d2[player]++;
-       }
+        player--; // so that it becomes 0 indexed
+        rows[row][player]++;
+        cols[col][player]++;
+        if (row == col) {
+            d1[player]++;
+        }
+        if (row + col == n - 1) {
+            d2[player]++;
+        }
 
-
-       //check win
-       if(rows[row][player]==n||
-            cols[col][player]==n||
-            d1[player]==n||
-            d2[player]==n){
-                return player+1; // 1 indexed again
-            }
-            return 0;
+        // check win
+        if (rows[row][player] == n || cols[col][player] == n ||
+            d1[player] == n || d2[player] == n) {
+            return player + 1; // 1 indexed again
+        }
+        return 0;
     }
 };
 
