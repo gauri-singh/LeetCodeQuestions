6c6d8501fe217bd4080ea99c382419013397af47
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-02-19 09:18:10 -0800

    Time: 0 ms (100%), Space: 21.7 MB (13.09%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+vector<int> boundary;
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(root==nullptr){
+            return boundary;
+        }
+        boundary.push_back(root->val);
+        isLeftBoundary(root->left);
+        leaves(root->left);
+        leaves(root->right);
+        isRightBoundary(root->right);
+        return boundary;
+    }
+    void leaves(TreeNode* root){
+        if(!root){
+            return;
+        }
+        if(!root->left && !root->right){
+            // leaf node found
+            boundary.push_back(root->val);
+            return;
+        }
+        //check for leaves in both subtrees
+        leaves(root->left);
+        leaves(root->right);
+    }
+    void isLeftBoundary(TreeNode* root){
+        if(!root || ( !root->left && !root->right)){
+            //should have atleast one child to proceed else leaf node so return
+            return;
+        }
+        // current node is in left
+        boundary.push_back(root->val);
+        if(root->left == nullptr){   
+            //check if right child could be the left boundary
+            isLeftBoundary(root->right);
+        }
+           //check if left child could be the left boundary
+        isLeftBoundary(root->left);
+    }
+      void isRightBoundary(TreeNode* root){
+        if(!root || ( !root->left && !root->right)){
+            //should have atleast one child to proceed else leaf node so return
+            return;
+        }
+      
+        if(root->right == nullptr){   
+            //check if left child could be the right boundary
+            isRightBoundary(root->left);
+        }
+           //check if left child could be the right boundary
+        isRightBoundary(root->right);
+        // current node is in right boundary
+        boundary.push_back(root->val);
+    }
+};
