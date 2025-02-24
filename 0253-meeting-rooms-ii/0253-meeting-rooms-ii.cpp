5c3d352511ff82783b8519a3180b0e6abc0052c3
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-02-23 16:12:50 -0800

    Time: 2 ms (65.36%), Space: 16.8 MB (34.44%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -24,3 +24,25 @@ public:
         return res;
     }
 };
+
+// //Heap solution
+// class Solution {
+// public:
+//     int minMeetingRooms(vector<vector<int>>& intervals) {
+//         vector<int> start,end;
+//         priority_queue<int,vector<int>, greater<int>> minheap; // minheap to get the earliest end times
+//         for(auto interval: intervals){
+//             start.push_back(interval[0]);
+//            minheap.push(interval[1]);
+//         }
+//         sort(start.begin(),start.end());
+//         for( auto startI : start){
+//             if(startI>=minheap.top()){
+//                 //not requiring a new room;
+//                 minheap.pop();
+//             }
+//         }
+//         //remaining end intervals in the heap = ongoing overlapping meetings = number of rooms required
+//         return minheap.size();
+//     }
+// };
