98106ec6259139350e9fb78870391c0ed71635da
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-01 10:32:39 -0800

    Time: 150 ms (28.59%), Space: 16.9 MB (68.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-vector<vector<bool>> dp;  
+    vector<vector<bool>>dp;
     bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0){
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        bool ans;
+        if (sum % 2 != 0) {
             return false; // cant divide odd size array into 2
         }
-        // if even then we can divide the number by 2 and see if sumhalf is available , if so then other half would also be available since sum is cumulative sum of all numbers in the array
-        sum=sum/2;
-        int n=nums.size();
-        dp=vector<vector<bool>>(n+1,vector<bool>(sum+1,false));
-        //initialization
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(i==0){
+        // if even then we can divide the number by 2 and see if sumhalf is
+        // available , if so then other half would also be available since sum
+        // is cumulative sum of all numbers in the array
+        sum = sum / 2;
+        int n = nums.size();
+        dp=vector<vector<bool>>(n + 1, vector<bool>(sum + 1, false));
+        return isSubsetSum(nums,sum);
+    }
+    //subset sum: 
+    bool isSubsetSum(vector<int>& arr, int target) {
+        int n=arr.size();
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<target+1;j++){
+                 if(i==0){
                     dp[i][j]=false;
                 }
                 if(j==0){
@@ -21,21 +28,20 @@ vector<vector<bool>> dp;
                 }
             }
         }
-        return subsetSum(nums,sum,n);
-    }
-    bool subsetSum(vector<int> & nums, int sum, int n){
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(nums[i-1]<=j){
-                    // the array is less than the sum so we can select it
-                    dp[i][j]= dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    //either take the number or dont take it
-                    //if youre taking thenumber, we are reducing it from sum so we can find remaining numbers
-                }else{
-                     dp[i][j]= dp[i-1][j];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<target+1;j++){
+                if(arr[i-1]<=j){
+                    //we can choose this element
+                    dp[i][j]=dp[i-1][j-arr[i-1]]|| dp[i-1][j];
+                    //taking or not taking the element
+                    //if taking we are reducing the current sum to be sum-currNumber
+                }
+                else{
+                    // we cannot choose this element
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        return dp[n][sum];
+        return dp[n][target];
     }
 };
