9fdd13581aa15b2a4db22fcf85457a4c3a7bdd50
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-07 14:36:37 -0800

    Time: 0 ms (100%), Space: 11.8 MB (21.49%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,47 +1,71 @@
 class Solution {
 public:
-//recursion
+// better way:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        if(nums.empty()){
-            //since we are popping one element at a time
-            return {{}};
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(curr,ans,nums);
+        return ans;
+    }
+    void backtrack(vector<int>& curr, vector<vector<int>>& ans, vector<int> nums){
+        //base case
+        if(curr.size()==nums.size()){
+            //we have found one permutation
+            ans.push_back(curr);
+            return;
         }
-        //remove first element from nums and call permute function recursively so we cn get every permutation of all elements except first one
-        //then add first one at every possible position in each of these permutations;
-        vector<int> temp(nums.begin()+1,nums.end()); //pop nums[0]
-        vector<vector<int>> perms= permute(temp);
-
-        for(auto p: perms){
-            //each p is a permutation vector
-            for(int i=0;i<=p.size();i++){
-                //<= p size so we can insert in end of current vector also
-                vector<int> p_copy=p;
-                p_copy.insert(p_copy.begin()+i,nums[0]); //insert the first element at every ith index
-                res.push_back(p_copy);// push this new permutation in result;
+        for(int i=0;i<nums.size();i++){
+            if(find(curr.begin(),curr.end(),nums[i])==curr.end()){
+                //the current number is not in ther permutation, so we can add and check if it completes
+                curr.push_back(nums[i]);
+                backtrack(curr,ans,nums);
+                curr.pop_back();
             }
         }
-        return res;
     }
-//iteration
-    // vector<vector<int>> permute(vector<int>& nums) {
-    //     vector<vector<int>> perms={{}};
+// //recursion
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> res;
+//         if(nums.empty()){
+//             //since we are popping one element at a time
+//             return {{}};
+//         }
+//         //remove first element from nums and call permute function recursively so we cn get every permutation of all elements except first one
+//         //then add first one at every possible position in each of these permutations;
+//         vector<int> temp(nums.begin()+1,nums.end()); //pop nums[0]
+//         vector<vector<int>> perms= permute(temp);
 
-    //     for(auto n: nums){
-    //         //for each number we want to add it to existing permutation
-    //         vector<vector<int>> new_perms;
-    //         for(auto p: perms){
-    //             // p is each individual permutation vector
-    //             //insert the number n in each of the possible places in p
-    //             for(int i=0;i<=p.size();i++){
-    //                 //<= to size cause we also want to insert at the end of the current permutation
-    //                 vector<int> p_copy=p;
-    //                 p_copy.insert(p_copy.begin()+i,n);
-    //                 new_perms.push_back(p_copy);
-    //             }
-    //         }
-    //         perms=new_perms;
-    //     }
-    //     return perms;
-    // }
+//         for(auto p: perms){
+//             //each p is a permutation vector
+//             for(int i=0;i<=p.size();i++){
+//                 //<= p size so we can insert in end of current vector also
+//                 vector<int> p_copy=p;
+//                 p_copy.insert(p_copy.begin()+i,nums[0]); //insert the first element at every ith index
+//                 res.push_back(p_copy);// push this new permutation in result;
+//             }
+//         }
+//         return res;
+//     }
+// //iteration
+//     // vector<vector<int>> permute(vector<int>& nums) {
+//     //     vector<vector<int>> perms={{}};
+
+//     //     for(auto n: nums){
+//     //         //for each number we want to add it to existing permutation
+//     //         vector<vector<int>> new_perms;
+//     //         for(auto p: perms){
+//     //             // p is each individual permutation vector
+//     //             //insert the number n in each of the possible places in p
+//     //             for(int i=0;i<=p.size();i++){
+//     //                 //<= to size cause we also want to insert at the end of the current permutation
+//     //                 vector<int> p_copy=p;
+//     //                 p_copy.insert(p_copy.begin()+i,n);
+//     //                 new_perms.push_back(p_copy);
+//     //             }
+//     //         }
+//     //         perms=new_perms;
+//     //     }
+//     //     return perms;
+//     // }
+
 };
