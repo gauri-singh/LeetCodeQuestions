189aa16555f06d002a3c61d77cc75593f74b3387
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-08 11:18:55 -0800

    Time: 0 ms (100%), Space: 10.1 MB (89.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,7 +5,7 @@ int rob(vector<int>& nums) {
         int n=nums.size();
         if (n == 1) return nums[0];
         int prev2=nums[0];
-        int prev1=max(nums[0],nums[1]);
+        int prev1=max(prev2,nums[1]);
         for(int i=2;i<n;i++){
             int curr=max(nums[i]+prev2,prev1);
             prev2=prev1;
