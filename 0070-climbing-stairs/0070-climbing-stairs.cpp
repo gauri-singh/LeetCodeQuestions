c1cfaa133f637c3ce8d74a58410c5ee1ebfafedc
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-08 09:32:24 -0800

    Time: 0 ms (100%), Space: 8.6 MB (17.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,38 @@
+//tabulation
 class Solution {
 public:
-vector<int> dp;
-    int climbStairs(int n) {
-        dp=vector<int>(n+1,-1);
-        return solve(0,n);
-    }
-    int solve(int currStep, int n){
-        if(currStep==n){
-            return 1;//valid path found
+   int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        if(n==1){
+            return 1;
         }
-        if(currStep>n){
-            return 0;// valid path not found
+        dp[1]=1; //1 way to reach if n =1
+        dp[2]=2;// 2 ways to reach if n=2 
+        for(int i=3;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[currStep]!=-1){
-            return dp[currStep];
-        }
-        return dp[currStep]=solve(currStep+1,n)+solve(currStep+2,n);
+        return dp[n];
     }
 };
+
+// class Solution {
+// public:
+// // memoized: 
+// vector<int> dp;
+//     int climbStairs(int n) {
+//         dp=vector<int>(n+1,-1);
+//         return solve(0,n);
+//     }
+//     int solve(int currStep, int n){
+//         if(currStep==n){
+//             return 1;//valid path found
+//         }
+//         if(currStep>n){
+//             return 0;// valid path not found
+//         }
+//         if(dp[currStep]!=-1){
+//             return dp[currStep];
+//         }
+//         return dp[currStep]=solve(currStep+1,n)+solve(currStep+2,n);
+//     }
+// };
