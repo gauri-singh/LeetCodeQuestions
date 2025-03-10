fe71358b13c69d3acd76494c2eed1e944d611a8c
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-10 09:33:49 -0700

    Time: 23 ms (48.04%), Space: 46.4 MB (10.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -23,3 +23,55 @@ public:
         return dp[n][amount];
     }
 };
+//different way if we took index from start
+/*class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,0));
+        for(int j=0;j<amount+1;j++){
+            dp[n][j]=0;
+        }
+         for(int i=0;i<n+1;i++){
+            dp[i][0]=1;
+        }
+        int count=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<amount+1;j++){
+                if(coins[i]<=j){
+                    dp[i][j]=dp[i][j-coins[i]]+dp[i+1][j];
+                }else{
+                    dp[i][j]=dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][amount];
+    }
+};
+memoization
+class Solution {
+public:
+vector<vector<int>> dp;
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        dp=vector<vector<int>>(n+1,vector<int>(amount+1,-1));
+        return solve(0,amount,coins);
+    }
+    int solve(int index,int amount,vector<int>& coins){
+        if(index>=coins.size()){
+            return 0;
+        }
+        if(amount==0){
+            return 1; // we have found a way
+        }
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int count=0;
+        if(coins[index]<=amount){
+            //then we can take it and keep taking it else move on
+            count+= solve(index,amount-coins[index],coins)+solve(index+1,amount,coins);
+        }else{
+            count+= solve(index+1,amount,coins);
+        }
+        return dp[index][amount] = count;
+    }
+};*/
