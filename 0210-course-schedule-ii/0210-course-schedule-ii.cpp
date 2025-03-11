079ffe6355c23393795af221b8a9c3c22be701e1
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-10 22:11:19 -0700

    Time: 8 ms (26.57%), Space: 19.3 MB (15.06%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses,0);
+        vector<int> output;
+        unordered_map <int, vector<int>> adj;
+        // build adjacency list and indegree array
+        for (auto edges: prerequisites) {
+            int course = edges[0], prereq = edges[1];
+            adj[prereq].push_back(course); // prereq → course
+            indegree[course]++; // course depends on prereq
+        }
+        //add all the subjects with no prereqs i.e, 0 indegree to the queue;
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+         // Perform topological sort
+        while (!q.empty()) {
+            int zero = q.front();// get the element from queue
+            q.pop();// remove it from map
+            output.push_back(zero);// add it to ordering
+            //decrement the indegree of affected nodes
+            for (auto nextCourse: adj[zero]) {
+                indegree[nextCourse]--;
+                if (indegree[nextCourse] == 0) {
+                    q.push(nextCourse);
+                }
+            }
+        }
+
+        // If we couldn't take all courses, return an empty array
+        if (output.size() != numCourses) {
+            return {};
+        }
+
+        return output;
+    }
+};
+/*Neetcode:
+class Solution {
+public:
 unordered_map <int, vector<int>> preMap;
 unordered_set<int> visit,cycle;
 vector<int> output;
@@ -34,4 +76,4 @@ vector<int> output;
         output.push_back(subj);
         return true;
     }
-};
+};*/
