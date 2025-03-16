f27ade03646c703582d9e38dedff684ca9ad86c6
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-15 18:30:18 -0700

    Time: 0 ms (100%), Space: 12.7 MB (32.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,7 +5,7 @@ public:
         nums.push_back(INT_MIN);
         for(int i=1;i<nums.size();i++){
             if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return pos=i;
+                return i;
             }
         }
         return pos;
