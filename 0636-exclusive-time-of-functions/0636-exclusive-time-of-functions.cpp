eb7e51def0d5b0442a45dfcad95a8a5fdf7eddd2
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-03-22 18:19:06 -0700

    Time: 23 ms (20.13%), Space: 27.1 MB (16.61%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+stack<int> st;
+int prevTime=0;
+
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> soloTime(n,0);
+        for(auto log: logs){
+            helper(log,soloTime);
+        }
+        return soloTime;
+    }
+    void helper(string log,vector<int>& soloTime){
+        string token;
+        vector<string> tokens;
+        stringstream ss(log);
+        while(getline(ss,token,':')){
+            tokens.push_back(token);
+        }
+        int id= stoi(tokens[0]);
+        int timeStamp=stoi(tokens[2]);
+        if(tokens[1]=="start"){
+            //on start add the time the function ran
+            if(!st.empty()){
+                soloTime[st.top()]+=timeStamp-prevTime;
+            }
+            st.push(id);
+            prevTime=timeStamp;
+        }else{
+            //on end, add the time the function ran+1
+            soloTime[st.top()]+=timeStamp-prevTime+1; // because inclusive
+            st.pop();
+            prevTime=timeStamp+1;
+        }
+    }
+};
