2d7fef42130aaddb4043cf2c9132fa2f6975ee8b
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-04-02 20:06:38 -0700

    Time: 13 ms (46.03%), Space: 34.9 MB (66.25%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        //our binary search search space is from maxElement weight to sum of all weight
+        int maxCap=accumulate(weights.begin(),weights.end(),0);
+        int minCap=*max_element(weights.begin(),weights.end());
+        int l=minCap, r=maxCap,midCap,ans=0;
+        while(l<=r){
+            midCap=l+(r-l)/2;
+            int midDays=getNumberOfdays(weights,midCap);
+            //we need to take more number of days,i.e, reduce capacity
+            // using my binary search template but have to reverse the sign because of question
+            if(days>=midDays){
+                ans=midCap;
+                r=midCap-1;// look for smaller value
+            }
+            else{
+                l=midCap+1;
+            }
+        }
+        return ans;
+    }
+    int getNumberOfdays(vector<int>& weights, int capacity){
+        int days=1;
+        int currCap=0;
+        for(int i=0;i<weights.size();i++){
+            if(currCap+weights[i]>capacity){
+                days++;
+                currCap=weights[i];
+                continue;
+            }
+            currCap+=weights[i];
+        }
+        return days;
+
+    }
+};
