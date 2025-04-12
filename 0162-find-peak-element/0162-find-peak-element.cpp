5016c054856068f361ea634df034ed2048863264
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-04-12 13:33:58 -0700

    Time: 0 ms (100%), Space: 12.6 MB (32.66%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int pos=0;
+        nums.insert(nums.begin(),INT_MIN);
         nums.push_back(INT_MIN); // so that we have neighbor to check if the size of the array is just 2 
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
+        int l=1, r=nums.size()-2;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid-1; // because we have added a -infinity in the beginning and we need to send the og index
+            }
+            //move the window where the peak is higher
+            if(nums[mid+1]>nums[mid]){
+                l=mid+1;
+            }else{
+                r=mid-1;
             }
         }
-        return pos;
+        return 0;
     }
 };
