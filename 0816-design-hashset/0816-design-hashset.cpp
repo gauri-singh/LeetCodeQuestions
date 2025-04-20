42f1ad44d0af170906e445aba3f3c9d6b915b7fd
Gauri Singh <30751893+gauri-singh@users.noreply.github.com>
2025-04-19 17:54:03 -0700

    Time: 39 ms (26.5%), Space: 61.5 MB (20.79%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.cpp
@@ -0,0 +1,65 @@
+class Node{
+    public:
+    Node* next;
+    int key;
+    Node(int k): key(k), next(nullptr){}
+};
+class MyHashSet {
+private:
+int hashFunction(int key){
+    return key%hashSet.size();
+}
+public:
+vector<Node*> hashSet;
+    MyHashSet() {
+        hashSet.resize(10000);
+        //initialize each bucket with a dummy node;
+        for(auto& bucket : hashSet){
+            bucket= new Node(0);
+        }
+    }
+    
+    void add(int key) {
+        int hash=hashFunction(key);
+        Node* curr= hashSet[hash];
+        while(curr->next){
+            if(curr->next->key==key){
+                return;
+            }
+            curr=curr->next;
+        }
+        curr->next=new Node(key);
+    }
+    
+    void remove(int key) {
+        int hash=hashFunction(key);
+        Node* curr= hashSet[hash];
+        while(curr->next){
+            if(curr->next->key==key){
+                curr->next=curr->next->next;
+                return;
+            }
+            curr=curr->next;
+        }
+    }
+    
+    bool contains(int key) {
+        int hash=hashFunction(key);
+        Node* curr= hashSet[hash];
+        while(curr->next){
+            if(curr->next->key==key){
+                return true;
+            }
+            curr=curr->next;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
